3bd5311fe4695aacf37b032105e3c81ad9fcaad2
Furkan Cabbar <furkancabbar546@gmail.com>
2024-10-30 11:59:36 +0300

    debug added

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Applant',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         scaffoldBackgroundColor: Colors.green,
